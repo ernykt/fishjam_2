b76676ab2f16dd7ba6342cbd111871be3a330f86
Musa Eren YAKUT <m.eren.yakut@outlook.com>
2024-04-06 16:35:23 +0300

    most stable version

--- scripts/door_1.gd
+++ scripts/door_1.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var door_timer = $DoorCollisionTimer
+
 var can_rotate = true
 var in_position = false
 var disable_collision = false
@@ -18,22 +20,24 @@ func _process(delta):
 	if $DoorSprite.rotation_degrees < -90:
 		can_rotate = true
 		in_position = true
-		$DoorShape.disabled = false
+		#$DoorShape.disabled = false
 		if not disable_collision:
 			disable_collision = true
 			$DoorShape.disabled = true
 	if can_rotate and in_position and timer.is_stopped():
 		$DoorSprite.rotation += 1 * delta
 		disable_collision = false
+		$DoorShape.disabled = false
 	if $DoorSprite.rotation_degrees >= 0:
 		in_position = false
 		$Button.disabled = false
 
 func _on_detect_fish_body_entered(body):
-	$DoorShape.set_deferred("disabled", false)
+	#$DoorShape.set_deferred("disabled", false)
+	pass
 	
 func _on_button_pressed():
 	can_rotate = false
 	$Button.disabled = true
-	timer.start(3)
+	timer.start(2)
 	
